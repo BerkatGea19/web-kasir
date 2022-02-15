a0f2a31cdbcdb85364050d858251ac30bdf92130
BerkatGea19 <berkat.gea@mail.ukrim.ac.id>
2022-02-16 00:57:09 +0700

    upload project react js kasir app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import NavbarComponent from './components/NavbarComponent';
+
+function App() {
+  return (
+    <div className="App">
+    <NavbarComponent/>
+    <h2>Hallo</h2>
+    </div>
+  );
+}
+
+export default App;
